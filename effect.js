857a4ce9d7f332dc8c39e7320a311c10f65b79ab
sauravrawat014 <picmactv@gmail.com>
2025-10-26 04:16:05 +0530

    Update effect.js

--- effect.js
+++ effect.js
@@ -223,11 +223,11 @@ $('document').ready(function(){
 		var i;
 
 		function msgLoop (i) {
-			$("p:nth-child("+i+")").fadeOut('slow').delay(800).promise().done(function(){
+			$("p:nth-child("+i+")").fadeOut('slow').delay(1500).promise().done(function(){
 			i=i+1;
-			$("p:nth-child("+i+")").fadeIn('slow').delay(1000);
-			if(i==50){
-				$("p:nth-child(49)").fadeOut('slow').promise().done(function () {
+			$("p:nth-child("+i+")").fadeIn('slow').delay(2000);
+			if(i==19){
+				$("p:nth-child(19)").delay(3000).fadeOut('slow').promise().done(function () {
 					$('.cake').fadeIn('fast');
 				});
 				
